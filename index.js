8d0e0e8227edfeef54a922358bb5323bbfc056b4
Shreya Thigale <hello.shreyathigale@gmail.com>
2025-06-02 15:02:59 +0000

    corrected the minutes formula, reset button and milliseconds typo

--- index.js
+++ index.js
@@ -28,6 +28,8 @@ function reset() {
     startTime = 0;
     elapsedTime = 0;
     isRunning = false;
+
+    display.textContent = "00:00:00:00"
 };
 
 function update() {
@@ -35,15 +37,15 @@ function update() {
     elapsedTime = currentTime - startTime;
 
     let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
-    let minutes = Math.floor(elapsedTime - (1000 * 60) % 60);
+    let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
     let seconds = Math.floor(elapsedTime / 1000 % 60)
-    let miliseconds = Math.floor(elapsedTime % 1000 / 10);
+    let milliseconds = Math.floor(elapsedTime % 1000 / 10);
 
     hours = String(hours).padStart(2, "0");
     minutes = String(minutes).padStart(2, "0");
     seconds = String(seconds).padStart(2, "0");
-    miliseconds = String(miliseconds).padStart(2, "0");
+    milliseconds = String(milliseconds).padStart(2, "0");
 
-    display.textContent = `${hours} ${minutes} ${seconds} ${miliseconds} `;
+    display.textContent = `${hours}:${minutes}:${seconds}:${milliseconds} `;
 
 };
